558c5e3a67328e56c1dd190c4c29fa65da2af219
joel costa <joelcosta@cpejr.com.br>
2022-05-10 22:21:55 -0300

    login funcional

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,27 @@
+import react, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import React from 'react';
 import { Form, Button } from "react-bootstrap";
 import "./login.css";
 
 
+
+
 function Login(){
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  function login(){
+    if(email === "joelcarlosuf@gmail.com" && password=== "123")
+    {
+      alert("Bem vindo!!\n  " + email);
+      navigate("home");
+    }
+    else
+      alert("Dados incorretos\n");
+  }
+
 
   return(
     <div className='base'>
@@ -14,13 +32,19 @@ function Login(){
           </div>
           <div className='inputs'>
             <Form.Group className="mb-3" controlId="email">
-              <Form.Control type="email" placeholder="E-mail" />
+              <Form.Control 
+              type="email" 
+              placeholder="E-mail"
+              onChange={(event)=>setEmail(event.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="senha">
-              <Form.Control type="password" placeholder="senha" />
+              <Form.Control
+              type="password" 
+              placeholder="senha" 
+              onChange={(event)=>setPassword(event.target.value)} />
             </Form.Group>
             
-            <Button variant="dark">Login</Button>{' '}
+            <Button variant="primary" onClick={login}>Login</Button>{' '}
          </div>
         </Form>
       </div>
