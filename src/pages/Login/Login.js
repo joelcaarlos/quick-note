de8b0d0f8ccfffa06ad9f376610bac9cbd65b334
joel costa <joelcosta@cpejr.com.br>
2022-05-10 21:15:00 -0300

    HTML CSS

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Form, Button } from "react-bootstrap";
+import "./login.css";
+
 
 function Login(){
 
   return(
-    <div>
-      
+    <div className='base'>
+      <div className='container'>
+        <Form>
+          <div className='login'>
+            <img src="/images/Login.png"/>
+          </div>
+          <div className='inputs'>
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Control type="email" placeholder="E-mail" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="senha">
+              <Form.Control type="password" placeholder="senha" />
+            </Form.Group>
+            
+            <Button variant="dark">Login</Button>{' '}
+         </div>
+        </Form>
+      </div>
     </div>
   );
 }
